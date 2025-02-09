85d4174abcf3da4c149708775a78a836d6d7ace3
BlazeWasHere <blaze@dorime.org>
2025-02-09 12:22:37 +0000

    add ramen

---
+++ adapters/ramen.ts
@@ -0,0 +1,29 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://prod-launchpad-1155389709.asia-southeast1.run.app/v1/rewards?address={address}"
+);
+
+/**
+{
+  status: "OK",
+  data: {
+    amount: 0,
+    amount_in_ether: "0.000000000000000000",
+    rewards: 792360000000000000000,
+    rewards_in_ether: "792.360000000000000000"
+  }
+}
+   */
+export default {
+  fetch: async (address: string) => {
+    return (await (await fetch(API_URL.replace("{address}", address))).json())
+      .data;
+  },
+  points: (data: Record<string, string>) => ({
+    amount_in_ether: parseFloat(data.amount_in_ether),
+    rewards_in_ether: parseFloat(data.rewards_in_ether),
+  }),
+  total: (data: Record<string, string>) => parseFloat(data.rewards_in_ether),
+} as AdapterExport;
