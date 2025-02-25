75ff65f66b711f532ffe42b3b7cf233a3b1c20b8
BlazeWasHere <blaze@dorime.org>
2025-02-25 18:21:30 +0000

    add superform

---
+++ adapters/superform.ts
@@ -0,0 +1,36 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://www.superform.xyz/api/proxy/superrewards/exploration/leaderboard/cred/{address}"
+);
+
+/**
+{
+  $schema: "https://api.superform.xyz/schemas/UserCredLeaderboard.json",
+  current_user: {
+    cred: 57932.9261003624,
+    rank: 15253,
+    tier: "Bronze",
+    percent_complete: 28.28,
+  },
+  cred_tiers: [
+    { tier: "Ethereal", cred: 869766757.6176672, rank: 1, is_user: false },
+    { tier: "Mythic", cred: 289283729.8177169, rank: 10, is_user: false },
+    { tier: "Diamond", cred: 54370643.94241665, rank: 50, is_user: false },
+    { tier: "Onyx", cred: 11002000.936844787, rank: 250, is_user: false },
+    { tier: "Gold", cred: 1493962.0512288355, rank: 1250, is_user: false },
+    { tier: "Silver", cred: 204824.36505193752, rank: 6250, is_user: false },
+    { tier: "Bronze", cred: 57932.9261003624, rank: 15253, is_user: true },
+  ],
+};
+ */
+export default {
+  fetch: async (address: string) => {
+    return (await (await fetch(API_URL.replace("{address}", address))).json())
+      .current_user;
+  },
+  points: (data: Record<string, string | number>) => data,
+  total: (data: { cred: number }) => data.cred,
+  rank: (data: { rank: number }) => data.rank,
+} as AdapterExport;
