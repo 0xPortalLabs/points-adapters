87760b81bc84bd8c6d247820721717aa5e5df8bc
BlazeWasHere <blaze@dorime.org>
2025-03-09 22:43:40 +0000

    update resolv

--- adapters/resolv.ts
+++ adapters/resolv.ts
@@ -1,38 +1,69 @@
 import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
 
-// NOTE: API supports querying by date
-// https://api.fuul.xyz/api/v1/payouts/leaderboard?user_address=0xD2eE2776F34Ef4E7325745b06E6d464b08D4be0E&from=2025-01-27T00:00:00.000Z&to=2025-01-27T23:59:59.999Z
-const API_URL =
-  "https://api.fuul.xyz/api/v1/payouts/leaderboard?user_address={address}";
-const API_KEY =
-  "983ef6bfd983055a38fbc436251c98624cb8de5da4767d1b6830a36849171ac2";
+// NOTE: API for leaderboard
+// https://api.resolv.im/points/leaderboard?page=1
+const API_URL = await maybeWrapCORSProxy(
+  "https://api.resolv.im/points?address={address}"
+);
+const RANK_URL = await maybeWrapCORSProxy(
+  "https://api.resolv.im/points/leaderboard/slice?address={address}"
+);
 
 /*
 {
-    address: "0xd2ee2776f34ef4e7325745b06e6d464b08d4be0e",
-    total_amount: "30460531657",
-    affiliate_code: null,
-    rank: 3,
-    total_attributions: 68,
-}
+  totalPoints: 4350139.138155897915,
+  dailyPoints: 0,
+  boosts: {
+    total: 100,
+    epoch: { name: "Grand Epoch", value: 25 },
+    tickerIsResolv: 75,
+  },
+  dailyActivities: {
+    referralPoints: 0,
+    holdUsrEth: 0,
+    holdUsrBase: 0,
+    holdStUsrEth: 0,
+    holdRlpEth: 0,
+    [...]
+  }
+};
+
+{
+  requestedAddressIndex: 1,
+  rows: [
+    {
+      rank: 10542,
+      address: "0xc174b22096790d0e65cc3cb48c4b51e7ca1a99d9",
+      points: 4350521.695522954886,
+    },
+    {
+      rank: 10543,
+      address: "0x171c53d55b1bcb725f660677d9e8bad7fd084282",
+      points: 4350139.138155897915,
+    },
+  ],
+};
 */
 export default {
   fetch: async (address: string) => {
-    const res = await (
-      await fetch(API_URL.replace("{address}", address), {
-        headers: { Authorization: `Bearer ${API_KEY}` },
-      })
-    ).json();
-    return res.results[0];
+    const [data, rankData] = await Promise.all([
+      (await fetch(API_URL.replace("{address}", address))).json(),
+      (await fetch(RANK_URL.replace("{address}", address))).json(),
+    ]);
+
+    return { data, rankData };
   },
-  points: (data: Record<string, number | string>) => {
-    return {
-      total_amount: parseFloat(String(data?.total_amount)) || 0,
-      rank: data?.rank ?? 0,
-      total_attributions: data?.total_attributions ?? 0,
-    };
+  points: ({ data }: { data: { dailyActivities: Record<string, number> } }) =>
+    data.dailyActivities,
+  total: ({ data }: { data: { totalPoints: number } }) => data.totalPoints,
+  rank: ({
+    rankData,
+  }: {
+    rankData: { requestedAddressIndex?: number; rows: { rank: number }[] };
+  }) => {
+    return rankData.requestedAddressIndex
+      ? rankData.rows[rankData.requestedAddressIndex].rank
+      : 0;
   },
-  total: (data: { total_amount?: number }) =>
-    parseFloat(String(data?.total_amount)) ?? 0,
-  rank: (data: { rank?: number }) => data?.rank,
 } as AdapterExport;
