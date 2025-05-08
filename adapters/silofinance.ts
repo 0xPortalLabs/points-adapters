95b54407395db9a2168cfbd1098b69a85b10a2cb
BlazeWasHere <blaze@dorime.org>
2025-05-08 17:12:27 +0000

    update silofinance

--- adapters/silofinance.ts
+++ adapters/silofinance.ts
@@ -13,23 +13,23 @@ const LEADERBOARD_URL = await maybeWrapCORSProxy(
   topAccounts: [
     {
       points: 1561433924.937687,
-      address: "0xceaeab74d8dd1688640c463c20c8d7ea3994e177",
-      position: 1,
+      account: "0xceaeab74d8dd1688640c463c20c8d7ea3994e177",
+      rank: 1,
     },
     {
       points: 1285248658.687515,
-      address: "0x431e81e5dfb5a24541b5ff8762bdef3f32f96354",
-      position: 2,
+      account: "0x431e81e5dfb5a24541b5ff8762bdef3f32f96354",
+      rank: 2,
     },
     {
       points: 939406603.93684,
-      address: "0x2ae08196c5323f5cd1a792de2124914ff4704e03",
-      position: 3,
+      account: "0x2ae08196c5323f5cd1a792de2124914ff4704e03",
+      rank: 3,
     },
     {
       points: 502.22421864787,
-      address: "0xf4fe75926d607d43b074f8de38a49258773090f7",
-      position: 8746,
+      account: "0xf4fe75926d607d43b074f8de38a49258773090f7",
+      rank: 8746,
     },
   ],
   total: 11151,
@@ -45,9 +45,9 @@ export default {
 
     const position =
       leaderboard.topAccounts.find(
-        (x: { address: string }) =>
-          x.address.toLowerCase() === address.toLowerCase()
-      )?.position ?? 0;
+        (x: { account: string }) =>
+          x.account.toLowerCase() === address.toLowerCase()
+      )?.rank ?? 0;
 
     return { points, position };
   },
@@ -60,5 +60,5 @@ export default {
     "Total (Global) Points": points.totalPoints,
   }),
   total: ({ points }: { points: { userPoints: number } }) => points.userPoints,
-  rank: ({ position }: { position: number }) => position,
+  rank: ({ position }: { position: string }) => parseInt(position),
 } as AdapterExport;
