d2fb76ea88b7499af9532eeefe7ac304f1ddb8b9
Synth0x <129101452+Synth0x@users.noreply.github.com>
2025-05-13 23:07:50 +0200

    handling season-based point

--- adapters/debridge.ts
+++ adapters/debridge.ts
@@ -16,6 +16,16 @@ const API_URL = await maybeWrapCORSProxy(
   "season": 1
 }
   */
+interface DeBridgeResponse {
+  userRank: number;
+  totalPoints: number;
+  activeMultiplier: number;
+  finalMultiplier: number;
+  nft: number | null;
+  originMultiplier: number | null;
+  season: number;
+}
+
 export default {
   fetch: async (address: string) => {
     const url = API_URL.replace("{address}", address);
@@ -23,21 +33,29 @@ export default {
       (await fetch(url + "?season=1")).json(),
       (await fetch(url + "?season=2")).json(),
     ]);
-
     return { s1, s2 };
   },
-  data: ({ s1, s2 }: Record<string, Record<string, number | null>>) => {
-    const get = (obj: Record<string, number | null>) => ({
-      Rank: obj.userRank,
-      "Total Points": obj.totalPoints,
-      "Active Multiplier": obj.activeMultiplier,
-      "Final Multiplier": obj.finalMultiplier,
-      NFT: obj.nft,
-      "Origin Multiplier": obj.originMultiplier,
-    });
-
-    return { "Season 1": get(s1), "Season 2": get(s2) };
-  },
-  total: ({ s2 }: { s2: { totalPoints: number } }) => s2.totalPoints,
-  rank: ({ s2 }: { s2: { userRank: number } }) => s2.userRank,
+  data: ({ s1, s2 }: { s1: DeBridgeResponse; s2: DeBridgeResponse }) => ({
+    "Season 1": {
+      "Total Points": s1.totalPoints || 0,
+      "Active Multiplier": s1.activeMultiplier || 1,
+      "Final Multiplier": s1.finalMultiplier || 1,
+      "NFT Multiplier": s1.nft || 0,
+      "Origin Multiplier": s1.originMultiplier || 0,
+      Rank: s1.userRank || 0,
+    },
+    "Season 2": {
+      "Total Points": s2.totalPoints || 0,
+      "Active Multiplier": s2.activeMultiplier || 1,
+      "Final Multiplier": s2.finalMultiplier || 1,
+      "NFT Multiplier": s2.nft || 0,
+      "Origin Multiplier": s2.originMultiplier || 0,
+      Rank: s2.userRank || 0,
+    }
+  }),
+  total: ({ s1, s2 }: { s1: DeBridgeResponse; s2: DeBridgeResponse }) => ({
+    "Season 1": s1.totalPoints || 0,
+    "Season 2": s2.totalPoints || 0
+  }),
+  rank: ({ s2 }: { s2: DeBridgeResponse }) => s2.userRank || 0,
 } as AdapterExport;
