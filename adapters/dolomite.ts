b1c3ef80d549d572def1de3181cc875f154d4c71
BlazeWasHere <blaze@dorime.org>
2025-05-20 13:17:17 +0000

    fix dolomite

--- adapters/dolomite.ts
+++ adapters/dolomite.ts
@@ -4,46 +4,35 @@ import { checksumAddress } from "viem";
 
 import { maybeWrapCORSProxy } from "../utils/cors.ts";
 
-const API_URL = await maybeWrapCORSProxy(
-  "https://api.dolomite.io/milestones/{address}/mineral"
-);
 const AIRDROP_URL = await maybeWrapCORSProxy(
   "https://api.dolomite.io/airdrop/regular/{address}"
 );
 
-// {amount: number | null, rank: number | null}
 export default {
   fetch: async (address: string) => {
     address = checksumAddress(address as `0x${string}`);
 
-    const [milestones, airdrop] = await Promise.all([
-      (await fetch(API_URL.replace("{address}", address))).json(),
-      (await fetch(AIRDROP_URL.replace("{address}", address))).json(),
-    ]);
+    const milestones = await (
+      await fetch(AIRDROP_URL.replace("{address}", address))
+    ).json();
 
-    return { milestones, airdrop: airdrop.airdrop };
+    return milestones;
   },
   data: ({
-    milestones,
     airdrop,
   }: {
-    milestones: { amount: string; rank: number | null };
-    airdrop?: { amount: string; level_snapshot: number };
+    airdrop?: { amount: string; level_snapshot: number | null };
   }) => {
     return {
       Minerals: {
-        Amount: parseFloat(milestones.amount) ?? 0,
-        Rank: milestones.rank ?? 0,
-        Airdrop: airdrop?.amount ?? 0,
-        Level: airdrop?.level_snapshot ?? 0,
+        "Airdrop Amount": airdrop ? parseFloat(airdrop.amount) ?? 0 : 0,
+        "Level Snapshot": airdrop?.level_snapshot ?? 0,
       },
     };
   },
-  total: ({ milestones }: { milestones: { amount: string } }) => ({
-    Minerals: parseFloat(milestones.amount) ?? 0,
+  total: ({ airdrop }: { airdrop?: { amount: string } }) => ({
+    Minerals: airdrop ? parseFloat(airdrop.amount) ?? 0 : 0,
   }),
-  rank: ({ milestones }: { milestones: { rank: number | null } }) =>
-    milestones.rank,
   // If they have airdrop data then it is probably claimable.
   claimable: ({ airdrop }: { airdrop?: unknown }) => Boolean(airdrop),
   deprecated: () => ({
