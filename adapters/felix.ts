56b47ce994745107f0ab69ac978a792a51c9d992
Acil Djehiche <acil.djehiche@outlook.com>
2025-11-04 21:54:33 +0000

    added new felix adapter

---
+++ adapters/felix.ts
@@ -0,0 +1,38 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+import { getAddress } from "viem";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://www.usefelix.xyz/api/points/{address}"
+);
+
+export default {
+  fetch: async (address) => {
+    const response = await fetch(
+      API_URL.replace("{address}", getAddress(address))
+    );
+    return await response.json();
+  },
+  data: (data) => {
+    if (!data || !Array.isArray(data)) return {};
+
+    let totalPoints = 0;
+
+    data.forEach((entry) => {
+      if (entry.pointsEarned) {
+        totalPoints += entry.pointsEarned;
+      }
+    });
+
+    return totalPoints;
+  },
+  total: (data) => {
+    if (!data || !Array.isArray(data)) return 0;
+
+    const totalPoints = data.reduce(
+      (sum, entry) => sum + (entry.pointsEarned || 0),
+      0
+    );
+    return totalPoints;
+  },
+} as AdapterExport;
