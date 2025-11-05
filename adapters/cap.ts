c0cb97e46c2a0b7f21ae15b29835e52aca851608
Acil <154090784+ADjehiche@users.noreply.github.com>
2025-11-05 22:44:44 +0000

    added new adapter for cap (#22)

---
+++ adapters/cap.ts
@@ -0,0 +1,21 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://api.cap.app/v1/caps/account/{address}"
+);
+export default {
+  fetch: async (address: string) => {
+    const res = await fetch(API_URL.replace("{address}", address));
+    return await res.json();
+  },
+  data: (data: { rank: number; caps: string }) => {
+    if (!data) return {};
+    return { Caps: data.caps, rank: data.rank };
+  },
+  total: (data: { caps: string }) => {
+    const points = data ? Number(data.caps) : 0;
+    return { Caps: points };
+  },
+  rank: (data: { rank: number }) => data.rank,
+} as AdapterExport;
