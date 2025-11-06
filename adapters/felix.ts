90f8be84fb9d5d223d3e6ea418586cc474583519
Acil <154090784+ADjehiche@users.noreply.github.com>
2025-11-06 22:53:55 +0000

    Added a new adapter for felix protocol (#21)
    * added new felix adapter
    
    * Return totalPoints as an object
    
    * clean up
    
    * using reduce
    
    * uselss function
    
    * renaming back to airdrop-aggregator

---
+++ adapters/felix.ts
@@ -0,0 +1,31 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+import { getAddress } from "viem";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://www.usefelix.xyz/api/points/{address}"
+);
+
+export default {
+  fetch: async (address) => {
+    const response = await fetch(
+      API_URL.replace("{address}", getAddress(address))
+    );
+    return await response.json();
+  },
+  data: (data) => {
+    if (!data || !Array.isArray(data)) return {};
+    let epochObj: Record<string, number> = {};
+    data.forEach((entry) => {
+      if (entry.epochNumber && entry.pointsEarned) {
+        const epochKey = `Epoch #${entry.epochNumber}`;
+        epochObj[epochKey] = entry.pointsEarned;
+      }
+    });
+    return epochObj;
+  },
+  total: (data) => {
+    if (!data || !Array.isArray(data)) return 0;
+    return data.reduce((total, entry) => total + (entry.pointsEarned || 0), 0);
+  },
+} as AdapterExport;
