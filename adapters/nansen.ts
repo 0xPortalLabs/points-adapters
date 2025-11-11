fb2869b9f0f2165104d33c01c255e778dd2cb798
Acil Djehiche <acil.djehiche@outlook.com>
2025-11-11 15:45:30 +0000

    adding nansen adapter

---
+++ adapters/nansen.ts
@@ -0,0 +1,37 @@
+import { getAddress } from "viem";
+import { AdapterExport } from "../utils/adapter.ts";
+
+const API_URL = "https://app.nansen.ai/api/points-leaderboard";
+
+// 0x6E93Ebc8302890fF1D1BeFd779D1DB131eF30D4d - Testing address
+export default {
+  fetch: async (address: string) => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    return data.filter(
+      (obj: { evm_address: string }) => obj.evm_address === getAddress(address),
+    )[0];
+  },
+  data: (data: {
+    points: number;
+    rank: number;
+    tier: string;
+    is_eligible: boolean;
+  }) => {
+    if (!data) return {};
+    return {
+      points: data.points,
+      rank: data.rank,
+      tier: data.tier,
+      is_eligible: String(data.is_eligible),
+    };
+  },
+  total: (data: { points: number }) => {
+    if (!data) return 0;
+    return data.points;
+  },
+  rank: (data: { rank: number }) => {
+    if (!data) return 0;
+    return data.rank;
+  },
+} as AdapterExport;
