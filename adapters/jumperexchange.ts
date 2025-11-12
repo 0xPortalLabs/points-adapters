79bf1ce8056f7bd6cf40dc5366cf81dd94a602c0
Blaze <blaze@dorime.org>
2025-11-12 18:15:20 +0000

    update jumper referrer

--- adapters/jumperexchange.ts
+++ adapters/jumperexchange.ts
@@ -11,7 +11,7 @@ const LEADERBOARD_URL = await maybeWrapCORSProxy(
 export default {
   fetch: async (address: string) => {
     const headers = {
-      Referer: "https://jumper.exchange/",
+      Referer: "https://checkpoint.exchange/",
     };
     const [rewards, leaderboard] = await Promise.all([
       await fetch(API_URL.replace("{address}", address), { headers }),
