e220833511b32cc9c092444c50386b24c9f188b4
Acil <154090784+ADjehiche@users.noreply.github.com>
2025-11-17 21:23:30 +0000

    added pjrx protocol adapter (#35)
    * added pjrx protocol adapter
    
    * return 0 if address not found
    
    * return 0 if user not found

---
+++ adapters/pjrx.ts
@@ -0,0 +1,25 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://api.prjx.com/points/user?walletAddress={address}",
+);
+
+type API_RESPONSE = {
+  pointsTotal: number;
+  rank: number;
+};
+
+export default {
+  fetch: async (address: string) => {
+    const res = await fetch(API_URL.replace("{address}", address));
+
+    const data = await res.json();
+    if (data.error === "User not found") return { pointsTotal: 0, rank: 0 };
+
+    return data;
+  },
+  data: (_) => ({}),
+  total: (data: API_RESPONSE) => data.pointsTotal,
+  rank: (data: API_RESPONSE) => data.rank,
+} as AdapterExport;
