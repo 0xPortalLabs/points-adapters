33dc82b65973ec82483ed1b34d0d43f99133c13a
Acil Djehiche <acil.djehiche@outlook.com>
2025-11-17 17:53:33 +0000

    added pjrx protocol adapter

---
+++ adapters/pjrx.ts
@@ -0,0 +1,22 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://api.prjx.com/points/user?walletAddress={address}",
+);
+
+type API_RESPONSE = {
+  pointsTotal: number;
+  rank: number;
+};
+
+export default {
+  fetch: async (address: string) => {
+    const res = await fetch(API_URL.replace("{address}", address));
+
+    return await res.json();
+  },
+  data: (_) => ({}),
+  total: (data: API_RESPONSE) => data.pointsTotal,
+  rank: (data: API_RESPONSE) => data.rank,
+} as AdapterExport;
