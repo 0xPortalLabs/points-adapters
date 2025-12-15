82c497d5a43cda635a2b21e9e5c4238200a4c9e7
Acil Djehiche <acil.djehiche@outlook.com>
2025-12-15 23:35:10 +0000

    updated adapter

--- adapters/rip.ts
+++ adapters/rip.ts
@@ -4,55 +4,27 @@ import { maybeWrapCORSProxy } from "../utils/cors.ts";
 import { getAddress } from "viem";
 import { convertKeysToStartCase } from "../utils/object.ts";
 
-const API_URL = await maybeWrapCORSProxy("https://rips.app/api/users/{fid}");
-const leaderboard = await maybeWrapCORSProxy(
-  "https://rips.app/api/rankings?page=0&limit=20000",
+const API_URL = await maybeWrapCORSProxy(
+  "https://rips.app/api/users/{fid}/stats",
 );
 
 type DATA_TYPE = {
   stats: Record<string, number>;
-  points: Record<string, number | string>;
-  rank: number;
 };
 
 export default {
   fetch: async (address) => {
-    const header = {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjM1NWQ0M2JmLWM0YjQtNDVlMy04MmNhLThlYjI4YzY3MDllNSJ9.eyJpYXQiOjE3NjU4MzUwNTEsImlzcyI6Imh0dHBzOi8vYXV0aC5mYXJjYXN0ZXIueHl6IiwiZXhwIjoxNzY1ODM4NjUxLCJzdWIiOjE2MTE3MjEsImF1ZCI6InJpcHMuYXBwIn0.GFBnSs0UyKrMDzRDsZ2iDoQpaaVdd1gvF3-wMe74yQVJ76-g2BHHHVhWm6yzF9L-sF6iIxPnCYgRPSbt4p5yteEN3RQZMhe_9SCOiPlb41TfdaCoJoW0wePxvDP7HndBXQNDj1vDGy8aGxBXeQZ2UlObvFGeFpPQNh5uVorHxw2_F9-uAEinR2pDUQflAYTHEDqSKYfPHPsBBTuE0Yz1ZQ7K3PupxVa-g7pN9MprKnvaGbT8PDEnzwMkHiZ8h3XrYW-l76eQz1Nj-Octxz9cau6JDKOQRYS-x1mM_--VXxipJo6gUCC7tJ2AqIH4f_SkYKA__Sj6BfIqwvcnKERvQA",
-      },
-    };
     const fid = await fidFromAddress(getAddress(address));
-    const res_points = await fetch(
-      API_URL.replace("{fid}", String(fid)),
-      header,
-    );
-    const res_stats = await fetch(
-      API_URL.replace("{fid}", String(fid)) + "/stats",
-      header,
-    );
-    const rank = await fetch(leaderboard, header);
-    const rank_data = await rank.json();
+    const res_stats = await fetch(API_URL.replace("{fid}", String(fid)));
+    const data = await res_stats.json();
     return {
-      stats: (await res_stats.json()).stats,
-      points: await res_points.json(),
-      rank: rank_data.users.find(
-        (obj: Record<string, number | string>) => obj.fid === Number(fid),
-      ).rank,
+      stats: data.stats,
     };
   },
   data: (data: DATA_TYPE) => {
-    const { Id, Fid, profileImageUrl, ...rest } = data.points;
-    return convertKeysToStartCase({
-      ...rest,
-      ...data.stats,
-      Rank: data.rank,
-      isAdmin: data.points.isAdmin ? "Yes" : "No",
-    });
+    return convertKeysToStartCase(data.stats);
   },
   total: (data: DATA_TYPE) => ({
-    "Rips Points": data.points.ripsPoints,
+    "Total Rips": data.stats.totalRips,
   }),
-  rank: (data: DATA_TYPE) => data.rank,
 } as AdapterExport;
