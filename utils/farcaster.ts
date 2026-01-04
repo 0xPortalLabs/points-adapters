4f237d1027c4dc3717e3c2c1f7db6e1bd5cabc00
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-04 19:32:30 +0000

    added cors wrap

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -1,5 +1,6 @@
 import { createPublicClient, http, parseAbi } from "viem";
 import { optimism } from "viem/chains";
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
 
 const idRegistryAbi = parseAbi([
   "function idOf(address owner) view returns (uint256 fid)"
@@ -9,7 +10,9 @@ const ID_REGISTRY = "0x00000000fc6c5f01fc30151999387bb99a9f489b";
 
 const client = createPublicClient({
   chain: optimism,
-  transport: http("https://public-op-mainnet.fastnode.io")
+  transport: http(
+    await maybeWrapCORSProxy("https://public-op-mainnet.fastnode.io")
+  )
 });
 
 export async function fidFromCustodyAddress(owner: `0x${string}`) {
