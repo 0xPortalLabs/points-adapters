fdcec3a77afc0bcb640c5d3ac44a2267ff8af103
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-09 20:20:10 +0000

    removing ring and fixing kinetiq

--- adapters/kinetiq.ts
+++ adapters/kinetiq.ts
@@ -2,11 +2,11 @@ import type { AdapterExport } from "../utils/adapter.ts";
 
 // API now requires wallet signature
 export default {
-  fetch: async () => await Promise.resolve({}),
-  data: () => ({}),
-  total: () => 0,
-  claimable: () => false,
-  deprecated: () => ({
-    kPoints: 1760572800 // October 16th 2025 GMT
-  })
+  fetch: async (address) => await Promise.resolve({}),
+  data: (data) => ({}),
+  total: (data) => 0,
+  claimable: (data) => false,
+  deprecated: (data) => ({
+    kPoints: 1760572800, // October 16th 2025 GMT
+  }),
 } as AdapterExport;
