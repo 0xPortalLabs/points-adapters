dc9e812fe72d17a1ee678b51ee64645a018c2a7c
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-10 21:59:58 +0000

    empty params

--- adapters/kinetiq.ts
+++ adapters/kinetiq.ts
@@ -3,10 +3,10 @@ import type { AdapterExport } from "../utils/adapter.ts";
 // API now requires wallet signature
 export default {
   fetch: async (address) => await Promise.resolve({}),
-  data: (data) => ({}),
-  total: (data) => 0,
-  claimable: (data) => false,
-  deprecated: (data) => ({
+  data: () => ({}),
+  total: () => 0,
+  claimable: () => false,
+  deprecated: () => ({
     kPoints: 1760572800, // October 16th 2025 GMT
   }),
 } as AdapterExport;
