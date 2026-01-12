4f5a914daf285a857166e3da524c7dfa35751b35
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-12 22:31:49 +0000

    adding hypersurface

---
+++ adapters/hypersurface.ts
@@ -0,0 +1,31 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+
+import { maybeWrapCORSProxy } from "../utils/cors.ts";
+
+const API_URL = await maybeWrapCORSProxy(
+  "https://points-api.hypersurface.io/points/{address}/state/"
+);
+
+type API_RESPONSE = {
+  rank: {
+    place: number;
+    total: number;
+  };
+  state: {
+    value: number;
+  };
+};
+
+export default {
+  fetch: async (address) => {
+    const res = await fetch(API_URL.replace("{address}", address));
+    return await res.json();
+  },
+  data: (data: API_RESPONSE) => ({
+    Rank: data.rank.place,
+    "Total Participants": data.rank.total,
+    Points: data.state.value,
+  }),
+  total: (data: API_RESPONSE) => data.state.value,
+  rank: (data: API_RESPONSE) => data.rank.place,
+} as AdapterExport;
