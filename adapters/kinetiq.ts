848c9476c393b94e9e67f4945c146ce11e87c945
Evx <154090784+ADjehiche@users.noreply.github.com>
2026-01-12 00:57:41 +0000

    readd deprecated keys
    Co-authored-by: Blaze <blaze@dorime.org>

--- adapters/kinetiq.ts
+++ adapters/kinetiq.ts
@@ -3,8 +3,8 @@ import type { AdapterExport } from "../utils/adapter.ts";
 // API now requires wallet signature
 export default {
   fetch: async (address) => await Promise.resolve({}),
-  data: () => ({}),
-  total: () => 0,
+  data: () => ({ kPoints: {} }),
+  total: () => ({ kPoints: 0 }),
   claimable: () => false,
   deprecated: () => ({
     kPoints: 1760572800, // October 16th 2025 GMT
