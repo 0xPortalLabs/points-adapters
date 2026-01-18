11e60e62f35b8ab4e06d02f78ba40252e99b7e2c
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-18 16:36:34 +0000

    added timeout

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -8,19 +8,16 @@ const idRegistryAbi = parseAbi([
 
 const ID_REGISTRY = "0x00000000fc6c5f01fc30151999387bb99a9f489b";
 
+const options = {
+  timeout: 2000,
+};
 const client = createPublicClient({
   chain: optimism,
   transport: fallback(
     [
-      http("https://public-op-mainnet.fastnode.io"),
-      http("https://optimism-rpc.publicnode.com"),
-      http("https://endpoints.omniatech.io/v1/op/mainnet/public"),
-      http("https://1rpc.io/op"),
-      http("https://optimism.drpc.org"),
-      http("https://optimism-public.nodies.app"),
-      http(
-        "https://api-optimism-mainnet-archive.n.dwellir.com/2ccf18bf-2916-4198-8856-42172854353c"
-      ),
+      http("https://public-op-mainnet.fastnode.io", options),
+      http("https://optimism-rpc.publicnode.com", options),
+      http("https://endpoints.omniatech.io/v1/op/mainnet/public", options),
     ],
     {
       rank: false,
