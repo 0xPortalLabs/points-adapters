6452c349b66e0fee90b3bc8db2f6390cf777fc04
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-18 21:43:51 +0000

    removed unused import and type

--- adapters/rip.ts
+++ adapters/rip.ts
@@ -2,15 +2,10 @@ import { getFidFromCustodyAddress } from "../utils/farcaster.ts";
 import type { AdapterExport } from "../utils/adapter.ts";
 import { maybeWrapCORSProxy } from "../utils/cors.ts";
 import { getAddress } from "viem";
-import { convertKeysToStartCase } from "../utils/object.ts";
 
 const API_URL = await maybeWrapCORSProxy(
   "https://rips.app/api/users/{fid}/stats"
 );
-
-type DATA_TYPE = {
-  stats: Record<string, number>;
-};
 
 export default {
   fetch: async (address) => {
