375c85090ba47a369af5a516e32f3ab23c7a9a7e
Acil Djehiche <acil.djehiche@outlook.com>
2026-01-19 00:36:57 +0000

    adding rainbow

--- adapters/rainbow.ts
+++ adapters/rainbow.ts
@@ -1,15 +1,32 @@
 import type { AdapterExport } from "../utils/adapter.ts";
 import { maybeWrapCORSProxy } from "../utils/cors.ts";
+import { formatEther } from "viem";
 
 const API_URL = await maybeWrapCORSProxy(
   "https://metadata.p.rainbow.me/v1/graph"
 );
+
+type API_RESPONSE = {
+  earnings: { total: number };
+  rewards: {
+    total: string;
+    claimable: number | string;
+    claimed: string;
+  };
+  stats: {
+    position: {
+      unranked: boolean;
+      current: number;
+    };
+  };
+};
 
 export default {
   fetch: async (address) => {
     const res = await fetch(API_URL, {
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer cYTvNiT7WQaQXCXVsbBn7H9cyxboFCQ3",
       },
       method: "POST",
       body: JSON.stringify({
@@ -45,7 +62,27 @@ export default {
         variables: { address },
       }),
     });
-    console.log(await res.json());
-    return {};
+    const data = await res.json();
+    return data.data.points.user;
+  },
+  data: (data: API_RESPONSE) => {
+    const isUnranked = !!data.stats.position.unranked;
+    const rank = isUnranked ? 0 : data.stats.position.current;
+    const points = data.earnings.total;
+    const claimable = data.rewards.claimable;
+
+    return {
+      Points: points,
+      Rank: rank,
+      Unranked: isUnranked ? "Yes" : "No",
+      Claimable: claimable && claimable !== "0" ? "Yes" : "No",
+      Rewards: `${formatEther(claimable as bigint)} ETH`,
+    };
+  },
+  total: (data: API_RESPONSE) => Number(data.earnings.total),
+  rank: (data: API_RESPONSE) => data.stats.position.current,
+  claimable: (data: API_RESPONSE) => {
+    const claimable = data.rewards.claimable;
+    return claimable && claimable !== "0";
   },
 } as AdapterExport;
