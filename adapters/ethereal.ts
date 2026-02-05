2186c24dcb58a0cf0ff8df1bbfc41e0961973228
blaze_sous_chef <blazesouschef@example.com>
2026-02-05 08:04:17 -0500

    fix: return same data format as totals in data() function

--- adapters/ethereal.ts
+++ adapters/ethereal.ts
@@ -1,8 +1,8 @@
 import type { AdapterExport } from "../utils/adapter.ts";
 import { checksumAddress } from "viem";
-import { convertKeysToStartCase } from "../utils/object.ts";
 
-const API_URL = "https://api.ethereal.trade/v1/points/summary?address={address}";
+const API_URL =
+  "https://api.ethereal.trade/v1/points/summary?address={address}";
 
 type API_RESPONSE = {
   id: string;
@@ -33,33 +33,36 @@ export default {
       return {};
     }
 
-    const info = data.reduce((acc, season) => {
-      const {
-        season: seasonNum,
-        address,
-        id,
-        updatedAt,
-        createdAt,
-        ...rest
-      } = season;
-      const seasonObj = {
-        ...rest,
-        points: Number(season.totalPoints) + Number(season.referralPoints),
-      };
+    const s1Data = data.find((item) => item.season === 1);
+    const s0Data = data.find((item) => item.season === 0);
 
-      return {
-        ...acc,
-        ...convertKeysToStartCase(
-          Object.fromEntries(
-            Object.entries(seasonObj).map(([key, value]) => [
-              `S${seasonNum}${key.charAt(0).toUpperCase() + key.slice(1)}`,
-              value,
-            ])
-          )
-        ),
-      };
-    }, {});
-    return info;
+    const season1 = s1Data
+      ? {
+        "S1 Total Points": Number(s1Data.totalPoints),
+        "S1 Previous Total Points": Number(s1Data.previousTotalPoints),
+        "S1 Referral Points": Number(s1Data.referralPoints),
+        "S1 Previous Referral Points": Number(s1Data.previousReferralPoints),
+        "S1 Rank": s1Data.rank,
+        "S1 Previous Rank": s1Data.previousRank,
+        "S1 Tier": s1Data.tier,
+        "S1 Points": Number(s1Data.totalPoints) + Number(s1Data.referralPoints),
+      }
+      : {};
+
+    const season0 = s0Data
+      ? {
+        "S0 Total Points": Number(s0Data.totalPoints),
+        "S0 Previous Total Points": Number(s0Data.previousTotalPoints),
+        "S0 Referral Points": Number(s0Data.referralPoints),
+        "S0 Previous Referral Points": Number(s0Data.previousReferralPoints),
+        "S0 Rank": s0Data.rank,
+        "S0 Previous Rank": s0Data.previousRank,
+        "S0 Tier": s0Data.tier,
+        "S0 Points": Number(s0Data.totalPoints) + Number(s0Data.referralPoints),
+      }
+      : {};
+
+    return { ...season1, ...season0 };
   },
   total: (apiData: { data: API_RESPONSE[] }) => {
     const s1Data = apiData.data.find((item) => item.season === 1);
