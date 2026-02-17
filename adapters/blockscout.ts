53010a4fb7d2bc3f727c6309dd793be7bf3185c7
Evx <154090784+ADjehiche@users.noreply.github.com>
2026-02-17 22:34:12 +0000

    adding blockscout (#74)
    * adding blockscout
    
    * remove useless header
    
    * adding ua and defaults

---
+++ adapters/blockscout.ts
@@ -0,0 +1,43 @@
+import type { AdapterExport } from "../utils/adapter.ts";
+import { convertKeysToStartCase } from "../utils/object.ts";
+
+const API_URL =
+  "https://merits-staging.blockscout.com/api/v1/leaderboard/users/{address}";
+
+type API_RESPONSE = {
+  total_balance: string;
+  referrals: string;
+  rank: string;
+  users_below: string;
+  top_percent: string;
+  address: string;
+};
+export default {
+  fetch: async (address) => {
+    const res = await fetch(API_URL.replace("{address}", address), {
+      headers: {
+        "User-Agent": "Checkpoint API (https://checkpoint.exchange)",
+      },
+    });
+    const data = await res.json();
+    if (data.message === "user not found") {
+      return {
+        total_balance: "0",
+        referrals: "0",
+        rank: "0",
+        users_below: "0",
+        top_percent: "0",
+      };
+    }
+    return data;
+  },
+  data: (data: API_RESPONSE) => {
+    const { total_balance, address, ...rest } = data;
+    return {
+      Merits: total_balance,
+      ...convertKeysToStartCase(rest),
+    };
+  },
+  total: (data: API_RESPONSE) => ({ Merits: Number(data.total_balance) }),
+  rank: (data: API_RESPONSE) => Number(data.rank),
+} as AdapterExport;
