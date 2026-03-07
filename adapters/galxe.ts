ca9d500671a6c89fe7d303e99696ab5dee07643a
Blaze <blaze@dorime.org>
2026-03-07 18:50:20 +0000

    fix galxe

--- adapters/galxe.ts
+++ adapters/galxe.ts
@@ -3,19 +3,21 @@ import type { AdapterExport } from "../utils/adapter.ts";
 import { maybeWrapCORSProxy } from "../utils/cors.ts";
 
 const API_URL = await maybeWrapCORSProxy(
-  "https://graphigo.prd.galaxy.eco/query",
+  "https://graphigo.prd.galaxy.eco/query"
 );
 
 type API_RESPONSE = {
   addressInfo: {
+    username: string;
     userXPLevel: {
       validXP: number;
       level: number;
       levelName: string;
-      nextLevelRequiredXP: number;
     };
   };
 };
+
+const userNotFound = (data: API_RESPONSE) => data.addressInfo.username === "";
 
 export default {
   fetch: async (address: string): Promise<API_RESPONSE> => {
@@ -28,30 +30,39 @@ export default {
       body: JSON.stringify({
         operationName: "AddressInfoXP",
         query:
-          "query AddressInfoXP($address: String!) {\n  addressInfo(address: $address) {\n    userXPLevel {\n      validXP\n      level\n      levelName\n      nextLevelRequiredXP\n    }\n    __typename\n  }\n}",
+          "query AddressInfoXP($address: String!) {\n  addressInfo(address: $address) {\n    username\n    userXPLevel {\n      validXP\n      level\n      levelName\n    }\n  }\n}",
         variables: {
           address,
         },
       }),
     });
     const data = await res.json();
-    if (data?.errors) throw new Error(data.errors);
+    if (data?.errors) throw new Error(JSON.stringify(data.errors));
 
     return data.data;
   },
   data: (data: API_RESPONSE) => {
+    if (userNotFound(data)) {
+      return {
+        XP: {
+          "Valid XP": 0,
+          Level: 0,
+          "Level Name": "N/A",
+        },
+      };
+    }
+
     const xpLevel = data.addressInfo.userXPLevel;
     return {
       XP: {
         "Valid XP": xpLevel.validXP,
         Level: xpLevel.level,
         "Level Name": xpLevel.levelName,
-        "Next Level Required XP": xpLevel.nextLevelRequiredXP,
       },
     };
   },
   total: (data: API_RESPONSE) => ({
-    XP: data.addressInfo.userXPLevel.validXP,
+    XP: userNotFound(data) ? 0 : data.addressInfo.userXPLevel.validXP,
   }),
   supportedAddressTypes: ["evm"],
 } as AdapterExport;
