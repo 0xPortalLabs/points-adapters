a4eb07e40e819081fc58fff001a27fbe8c4e9c57
Blaze <blaze@dorime.org>
2026-05-10 18:41:21 +0000

    fix doma, lombard, rainbow, sierra, treehouse

--- adapters/lombard.ts
+++ adapters/lombard.ts
@@ -4,28 +4,13 @@ import { isEmpty } from "lodash-es";
 import { convertKeysToStartCase } from "../utils/object.ts";
 
 const API_URL = await maybeWrapCORSProxy(
-  "https://mainnet.prod.lombard.finance/api/v1/referral-system/season-2/points/{address}"
+  "https://mainnet.prod.lombard.finance/api/v1/referral-system/season-3/points/{address}"
 );
 
-// {
-//     "holding_points": 0.015005404,
-//     "protocol_points": 21.982641,
-//     "total": 1921.9977,
-//     "protocol_points_map": {
-//         "lombard-holding-points-eth": 0.015005404,
-//         "lombard-sonic-vault-eth": 21.982641
-//     },
-//     "badge_points": 1500,
-//     "checkin_points": 400
-// }
 type API_RESPONSE = {
-  holding_points: number;
-  protocol_points: number;
-  total: number;
   protocol_points_map: Record<string, number>;
-  badge_points: number;
-  checkin_points: number;
 };
+
 export default {
   fetch: async (address: string) => {
     const response = await fetch(API_URL.replace("{address}", address), {
@@ -34,27 +19,24 @@ export default {
       },
     });
     const data = await response.json();
+
     // API returns an empty object when address doesn't exist
     if (!data || isEmpty(data)) {
       return {
-        holding_points: 0,
-        protocol_points: 0,
-        total: 0,
         protocol_points_map: {},
-        badge_points: 0,
-        checkin_points: 0,
       };
     }
     return data;
   },
-  data: (data: API_RESPONSE) => {
-    return {
-      "Total Points": data.total,
-      ...convertKeysToStartCase(data.protocol_points_map),
-      "Badge Points": data.badge_points,
-      "Tap In Points": data.checkin_points,
-    };
-  },
-  total: (data: API_RESPONSE) => data.total,
+  data: (data: API_RESPONSE) => ({
+    Lux: { ...convertKeysToStartCase(data.protocol_points_map) },
+  }),
+  total: (data: API_RESPONSE) => ({
+    Lux: Object.values(data.protocol_points_map).reduce(
+      (sum, points) => sum + points,
+      0
+    ),
+  }),
+  claimable: () => true,
   supportedAddressTypes: ["evm"],
 } as AdapterExport;
--- adapters/treehouse.ts
+++ adapters/treehouse.ts
@@ -1,77 +1,21 @@
 import type { AdapterExport } from "../utils/adapter.ts";
 
-import { startCase } from "lodash-es";
-
-const API_URL =
-  "https://api.treehouse.finance/gonuts-s2/weekly-points?address={address}";
-const S1_API_URL =
-  "https://api.treehouse.finance/gonuts-s1/nuts-accrued?address={address}";
-
-// {"message":"success","total_points_accrued_s1":8764.180039}
-/*
-  data: [
-    {
-      wallet_address: "0x69155e7ca2e688ccdc247f6c4ddf374b3ae77bd6",
-      total_weekly_points: 0.0010500000604928101,
-      week_start_date: "2025-07-14",
-      week_end_date: "2025-07-20",
-    },
-  ],
-  message: "success",
-  total_points_accrued: 0.007350000423449671,
-};
-*/
+// Points ended completely
+// ref: https://x.com/TreehouseFi/status/2049383503440281909
 export default {
-  fetch: async (address: string) => {
-    const headers = {
-      "User-Agent": "Checkpoint API (https://checkpoint.exchange)",
-    };
-
-    const [s1, s2] = await Promise.all([
-      (
-        await fetch(S1_API_URL.replace("{address}", address), { headers })
-      ).json(),
-      (await fetch(API_URL.replace("{address}", address), { headers })).json(),
-    ]);
-
-    return { s1, s2 };
-  },
-  data: ({
-    s1,
-    s2,
-  }: {
-    s1: Record<string, unknown>;
-    s2: Record<string, unknown>;
-  }) => {
-    const s2Data: Record<string, string | number> = {};
-    if (s2?.data && Array.isArray(s2.data)) {
-      s2.data.forEach((obj: Record<string, unknown>, idx: number) => {
-        delete obj.wallet_address;
-
-        Object.entries(obj).forEach(([k, v]) => {
-          s2Data[`Week ${idx + 1} ${startCase(k)}`] = v as string | number;
-        });
-      });
-    }
-
-    return {
-      "S1 Nuts": {
-        "Total Points": s1.total_points_accrued_s1 as string | number,
-      },
-      "S2 Weekly Points": s2Data,
-    };
-  },
-  total: ({
-    s1,
-    s2,
-  }: {
-    s1: Record<string, unknown>;
-    s2: Record<string, unknown>;
-  }) => {
-    return {
-      "S1 Nuts": s1?.total_points_accrued_s1,
-      "S2 Weekly Points": s2?.total_points_accrued,
-    };
-  },
+  fetch: () => Promise.resolve({}),
+  data: () => ({
+    "S1 Nuts": {},
+    "S2 Weekly Points": {},
+  }),
+  total: () => ({
+    "S1 Nuts": 0,
+    "S2 Weekly Points": 0,
+  }),
+  claimable: () => true,
+  deprecated: () => ({
+    "S1 Nuts": 1777446077, // Apr 29th 07:01 UTC
+    "S2 Weekly Points": 1777446077, // Apr 29th 07:01 UTC
+  }),
   supportedAddressTypes: ["evm"],
 } as AdapterExport;
